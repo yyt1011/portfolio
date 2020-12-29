bfdd04c67fa86580405bf81e738eef6272950fd7
yyt1011 <35545773+yyt1011@users.noreply.github.com>
2020-12-29 14:15:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dc2c08d603a180a95c67ac9a066b4d40.js"
+  "/portfolio/precache-manifest.97707334502008b9e2f07a7d1ff5d5d8.js"
 );
 
 self.addEventListener('message', (event) => {
